3f5fb89167861531a52ed713af7c0374b4cc000b
BW <supopopo@naver.com>
2012-08-09 19:18:51 +0900

    splash추가
    splash 에서 일정시간뒤 MainActivity로 가게 코딩해놈

---
+++ T/src/com/pr/T/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.pr.T;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	 requestWindowFeature(Window.FEATURE_NO_TITLE);
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.splash);
+         
+         Handler x = new Handler();
+         x.postDelayed(new splashhandler(), 2000); //1.5초뒤에 다른 엑티비티로...
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
+    
+    class splashhandler implements Runnable {
+        @Override
+        public void run() {
+			Intent intent1 = new Intent(SplashActivity.this, MainActivity.class);
+			startActivity(intent1);
+
+            finish();
+        }        
+    }
+}
