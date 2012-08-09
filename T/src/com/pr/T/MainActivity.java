3f3683a207964e8c5275891a27b716adcc697fe3
BW <supopopo@naver.com>
2012-08-09 19:17:36 +0900

    splash.java .xml 추가

--- T/src/com/pr/T/MainActivity.java
+++ T/src/com/pr/T/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
 	@Override
-	public void onCreate(Bundle ds) {
-		super.onCreate(ds);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 	}
